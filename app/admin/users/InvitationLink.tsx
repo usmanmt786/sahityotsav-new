92af71daa231924435b23cc07a61312777d70cda
fathah-work <abdulfathahkodag@gmail.com>
2024-08-10 10:23:49 +0400

    Links Page BG

--- app/admin/users/InvitationLink.tsx
+++ app/admin/users/InvitationLink.tsx
@@ -7,7 +7,7 @@ import { MdContentCopy } from "react-icons/md";
 const InvitationLink = () => {
     const link = `https://${APPCONFIGS.domain}/invitation`;
     return (
-        <section className="p-5 bg-primary bg-opacity-30  mt-6 mb-3 ring-2 ring-primaryDark inline-block">
+        <section className="p-5 bg-primary bg-opacity-10  mt-6 mb-3 ring-2 ring-primaryDark inline-block">
         <h4 className="text-xl font-bold">Note</h4>
         Invited user can accept the invitation using the following link
         <h6 
